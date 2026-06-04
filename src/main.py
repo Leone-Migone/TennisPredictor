f9446919bff1fd3801dfe25712f91adbcaf11f23
Leone Migone <migoneleone@gmail.com>
2026-06-04 03:05:48 +0100

    Add baseline model training to use linear regression ,and update README with project log

--- src/main.py
+++ src/main.py
@@ -1,11 +1,15 @@
 import pandas as pd
 from data_loader import load_data
 from preprocessing import create_model_data
+from train_model import train_baseline_model
 matches = load_data()
 matches.info()
 model_data = create_model_data(matches)
 model_data.info()
+
 print(model_data.head())
-print(model_data.shape)
 print(model_data["target"].value_counts())
+print(model_data.shape)
 print(model_data.isna().sum())
+
+model = train_baseline_model(model_data)
---
+++ src/train_model.py
@@ -0,0 +1,44 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+import sklearn.linear_model as lm
+import sklearn.metrics as metrics
+
+def train_baseline_model(model_data):
+    features = [
+        "rank_diff",
+        "age_diff",
+        "surface",
+        "best_of"
+    ]
+
+    target = "target"
+
+
+    x = model_data[features].copy()
+    y = model_data[target]
+
+    #convert string columns into numeric
+    x = pd.get_dummies(x, columns=["surface"], drop_first=True)
+
+    x_train, x_test, y_train, y_test = train_test_split(
+        x,y,test_size=0.2,random_state=1,stratify=y
+    )
+
+    model = lm.LogisticRegression(max_iter=1000)
+
+    model.fit(x_train,y_train)
+    y_pred = model.predict(x_test)
+    accuracy = metrics.accuracy_score(y_test,y_pred)
+    
+    print("Baseline Logistic Regression")
+    print("----------------------------")
+    print(f"Accuracy: {accuracy:.4f}")
+    print()
+    print("Confusion Matrix:")
+    print(metrics.confusion_matrix(y_test, y_pred))
+    print()
+    print("Classification Report:")
+    print(metrics.classification_report(y_test, y_pred))
+
+    return model
