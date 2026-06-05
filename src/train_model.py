0d0f137b22aead990d2369f0cf156e62cd63c612
Leone Migone <migoneleone@gmail.com>
2026-06-05 01:07:28 +0100

    Refactor model training to split data chronologically

--- src/train_model.py
+++ src/train_model.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
-import sklearn.linear_model as lm
+import sklearn as sk 
 import sklearn.metrics as metrics
 
 def train_baseline_model(model_data):
@@ -16,19 +16,25 @@ def train_baseline_model(model_data):
     ]
 
     target = "target"
+    #rather than splitting data for training randomly doing it cronologically its more logical
+    train_data = model_data[model_data["tourney_date"] < 20220101]
+    test_data = model_data[model_data["tourney_date"] >= 20220101]
 
+    x_train = train_data[features].copy()
+    y_train = train_data[target]
 
-    x = model_data[features].copy()
-    y = model_data[target]
+    x_test = test_data[features].copy()
+    y_test= test_data[target]
 
     #convert string columns into numeric
-    x = pd.get_dummies(x, columns=["surface"], drop_first=True)
-
-    x_train, x_test, y_train, y_test = train_test_split(
-        x,y,test_size=0.2,random_state=1,stratify=y
-    )
-
-    model = lm.LogisticRegression(max_iter=1000)
+    x_test= pd.get_dummies(x_test, columns=["surface"], drop_first=True)
+    x_train = pd.get_dummies(x_train, columns=["surface"], drop_first=True)
+    #That ensures the test set has the same columns as the training set.
+    x_test = x_test.reindex(columns=x_train.columns, fill_value=0)
+    #rf_model = sk.ensemble.RandomForestClassifier(n_estimators=100, random_state=1)
+    #rf_model.fit(x_train, y_train)
+    #y_pred = rf_model.predict(x_test)    
+    model = sk.linear_model.LogisticRegression(max_iter=1000)
 
     model.fit(x_train,y_train)
     y_pred = model.predict(x_test)
@@ -36,7 +42,7 @@ def train_baseline_model(model_data):
     
     print("Baseline Logistic Regression with H2H and rank points difference")
     print("----------------------------")
-    print(f"Accuracy: {accuracy:.4f}")
+    print(f"Accuracy: {accuracy*100:.4f}%")
     print()
     print("Confusion Matrix:")
     print(metrics.confusion_matrix(y_test, y_pred))
